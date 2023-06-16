7b38cf6363163fb3ea3652219104d13a0ff68ffd
ijaeu <lebitances@hanmail.net>
2023-06-16 13:46:59 +0900

    2차 fragment test 중

--- src/main/java/com/example/mealme/controller/meal/MealController.java
+++ src/main/java/com/example/mealme/controller/meal/MealController.java
@@ -1,11 +1,17 @@
 package com.example.mealme.controller.meal;
 
+import com.example.mealme.service.meal.MealService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/meal/*")
 @RequiredArgsConstructor
 public class MealController {
+    private final MealService mealService;
+
+    @GetMapping("/myPage")
+    public void myPage(){}
 }
