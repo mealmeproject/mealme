1de7339ce77b72feea07eda2d3928b0aabf13787
iosghz <iosghz785@naver.com>
2023-06-19 13:50:59 +0900

    06.19 최진성 shopping 부분

--- src/main/java/com/example/mealme/controller/shop/ShopController.java
+++ src/main/java/com/example/mealme/controller/shop/ShopController.java
@@ -4,6 +4,7 @@ import com.example.mealme.service.meal.MealService;
 import com.example.mealme.service.shop.ShopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class ShopController {
     private final ShopService shopService;
+
+    @GetMapping("/shoppingList")
+    public void shoppingList(){}
+
+    @GetMapping("/shoppingBasket")
+    public void shoppingBasket(){}
 
 }
