e381499157bb168546ab349965412d3444b20255
MichealChung <jst6227@naver.com>
2023-06-28 13:37:41 +0900

    0628 커밋이요

--- src/main/java/com/example/mealme/controller/company/CompanyController.java
+++ src/main/java/com/example/mealme/controller/company/CompanyController.java
@@ -2,10 +2,18 @@ package com.example.mealme.controller.company;
 
 import com.example.mealme.service.company.CompanyService;
 import com.example.mealme.service.meal.MealService;
+import com.example.mealme.vo.CompanyListVo;
+import com.example.mealme.vo.CompanyReviewVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/company/*")
@@ -17,10 +25,30 @@ public class CompanyController {
     public void largeCategory(){}
 
     @GetMapping("/hospitalCategory")
-    public void hospitalCategory(){}
+    public String hospitalCategory(@RequestParam("companyCodeNumber")Long companyCodeNumber, Model model){
+        String companyCodeName = null;
+        if(companyCodeNumber == 100){
+            companyCodeName = "병원";
+        } else if(companyCodeNumber == 200){
+            companyCodeName = "트레이너";
+        } else if(companyCodeNumber == 300){
+            companyCodeName = "영양사";
+        }
+        List<CompanyListVo> returnList = companyService.output(companyCodeNumber);
+        model.addAttribute("companies", returnList);
+        model.addAttribute("companyCodeName", companyCodeName);
+        return "company/hospitalCategory";
+    }
 
     @GetMapping("/detailedHospital")
-    public void detailedHospital(){}
+    public String detailedHospital(@RequestParam("companyNumber")Long companyNumber, Model model){
+        CompanyListVo detailedList = companyService.showDetail(companyNumber);
+        List<CompanyReviewVo> selectReviewList = companyService.showReview(companyNumber);
+        model.addAttribute("companyinfo", detailedList);
+        model.addAttribute("companyReviewList", selectReviewList);
+        model.addAttribute("companyNumber", companyNumber);
+        return "company/detailedHospital";
+    }
 
     @GetMapping("/settingThePeriod")
     public void settingThePeriod(){}
@@ -55,3 +83,5 @@ public class CompanyController {
     }
 
 }
+
+
--- src/main/java/com/example/mealme/service/company/CompanyService.java
+++ src/main/java/com/example/mealme/service/company/CompanyService.java
@@ -1,11 +1,29 @@
 package com.example.mealme.service.company;
 
+import com.example.mealme.mapper.CompanyMapper;
+import com.example.mealme.vo.CompanyListVo;
+import com.example.mealme.vo.CompanyReviewVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CompanyService {
+    private final CompanyMapper companyMapper;
+
+    public List<CompanyListVo> output(Long companyCodeNumber){
+        return companyMapper.selectAll(companyCodeNumber);
+    }
+
+    public CompanyListVo showDetail(Long companyNumber){
+        return companyMapper.selectDetail(companyNumber);
+    }
+
+    public List<CompanyReviewVo> showReview(Long companyNumber){
+        return companyMapper.selectReview(companyNumber);
+    }
 }
